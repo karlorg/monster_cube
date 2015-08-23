2ff438c1e9abb0fd8d996eabaa244554f44bf9b8
karlorg <karlorg@users.noreply.github.com>
2015-08-23 15:37:17 +0100

    advs stop and mime shooting

--- source/Adventurer.hx
+++ source/Adventurer.hx
@@ -6,27 +6,31 @@ import flixel.util.FlxAngle;
 import flixel.util.FlxColor;
 import flixel.util.FlxPath;
 import flixel.util.FlxPoint;
+import flixel.util.FlxRandom;
 
 enum Behavior {
     Idle;
     Running;
+    Shooting;
 }
 
 class Adventurer extends FlxSprite {
 
     static var speedIdle : Float = 0.3;
-    static var speedRun : Float = 0.9;
+    static var speedRun : Float = 1.1;
     static var tileWidth : Int = PlayState.tileWidth;
     static var tileHeight : Int = PlayState.tileHeight;
 
     public var path : FlxPath;
 
     private var behavior : Behavior;
+    private var lastShot : Int;
     private var lastPathed : Int;
     private var lastSawCube : Int;
     private var nodes : Array<FlxPoint>;
     private var player : Player;
     private var ticks : Int;
+    private var ticksToRun : Int;
     private var tilemap : FlxTilemap;
     private var treasure : FlxSprite;
 
@@ -56,6 +60,7 @@ class Adventurer extends FlxSprite {
         ticks = 0;
         lastPathed = -10000;
         lastSawCube = -10000;
+        ticksToRun = -1;
     }
 
     override public function update() : Void {
@@ -93,14 +98,67 @@ class Adventurer extends FlxSprite {
             }
 
             if (canSeePlayer()) {
-                this.behavior = Running;
+                this.behavior = Shooting;
+                lastShot = ticks;
                 lastSawCube = ticks;
                 lastPathed = -10000;  // force re-path
             }
 
         case Running:
-            if (ticks - lastPathed > 29) {
-                var anglePlayerToMe = FlxAngle.angleBetween(player, this);
+            if (ticksToRun < 0) {
+                ticksToRun = FlxRandom.intRanged(24, 34);
+            }
+            if (ticks - lastPathed > ticksToRun) {
+                ticksToRun = -1;
+                this.behavior = Shooting;
+                lastShot = ticks;
+            }
+            if (canSeePlayer()) {
+                lastSawCube = ticks;
+            // Check if we should stop running
+            } else if (ticks - lastSawCube > 8 * 60) {
+                behavior = Idle;
+            }
+
+            if (!path.finished && path.nodes != null) {
+                if (path.angle >= -45 && path.angle <= 45) {
+                    animation.play("up");
+                } else if (path.angle >= 135 || path.angle <= -135) {
+                    animation.play("down");
+                } else if (path.angle > 45 && path.angle < 135) {
+                    animation.play("right");
+                } else {
+                    animation.play("left");
+                }
+            } else {
+                animation.curAnim.curFrame = 0;
+                animation.curAnim.stop();
+                nodes = null;
+            }
+
+        case Shooting:
+            path.cancel();
+            var anglePlayerToMe = FlxAngle.angleBetween(player.cube, this,
+                                                        true);  // asDegrees
+            if (ticks - lastShot < 10) {
+                if (canSeePlayer()) {
+                    var faceAngle = anglePlayerToMe + 90 + 180;
+                    while (faceAngle > 180) { faceAngle -= 360; }
+                    if (faceAngle <= 45 && faceAngle >= -45) {
+                        animation.play("up");
+                    } else if (faceAngle >= 135 || faceAngle <= -135) {
+                        animation.play("down");
+                    } else if (faceAngle > 45 && faceAngle < 135) {
+                        animation.play("right");
+                    } else {
+                        animation.play("left");
+                    }
+                    animation.curAnim.curFrame = 0;
+                    animation.curAnim.stop();
+                }
+            } else {
+
+                // pick a new path and start running again
                 anglePlayerToMe = FlxAngle.getAngle(
                     startPoint, playerPoint) + 90;
                 function candidate(dist : Float,
@@ -113,28 +171,13 @@ class Adventurer extends FlxSprite {
                     return result;
                 }
                 var destPoint = startPoint;
-                for (point in [candidate(6, 0),
-                               candidate(5, 0),
-                               candidate(5, 10),
-                               candidate(5, -10),
-                               candidate(4, 0),
-                               candidate(4, 30),
-                               candidate(4, -30),
-                               candidate(4, 50),
-                               candidate(4, -50),
-                               candidate(3, 0),
-                               candidate(3, 50),
-                               candidate(3, -50),
-                               candidate(3, 70),
-                               candidate(3, -70),
-                               candidate(2, 0),
-                               candidate(2, 70),
-                               candidate(2, -70),
-                               candidate(2, 90),
-                               candidate(2, -90),
-                               candidate(1, 0),
-                               candidate(1, 90),
-                               candidate(1, -90)]) {
+                for (xy in [[6,0],
+                            [5,0], [5,10], [5,-10],
+                            [4,0], [4,30], [4,-30], [4,50], [4,-50],
+                            [3,0], [3,50], [3,-50], [3,70], [3,-70],
+                            [2,0], [2,70], [2,-70], [2,90], [2,-90],
+                            [1,0], [1,90], [1,-90]]) {
+                    var point = candidate(xy[0], xy[1]);
                     if (tilemap.getTile(Math.floor(point.x / tileWidth),
                                         Math.floor(point.y / tileHeight))
                         == 1) {
@@ -147,32 +190,9 @@ class Adventurer extends FlxSprite {
                     path.start(this, nodes, speedRun * 60);
                 }
                 lastPathed = ticks;
+                this.behavior = Running;
 
-                // Check if we should stop running
-                if (canSeePlayer()) {
-                    lastSawCube = ticks;
-                }
-                if (ticks - lastSawCube > 8 * 60) {
-                    behavior = Idle;
-                }
             }
-
-        }
-
-        if (!path.finished && path.nodes != null) {
-            if (path.angle <= 45 && path.angle >= -45) {
-                animation.play("up");
-            } else if (path.angle >= 135 && path.angle <= -135) {
-                animation.play("down");
-            } else if (path.angle > 45 && path.angle < 135) {
-                animation.play("right");
-            } else {
-                animation.play("left");
-            }
-        } else {
-            animation.curAnim.curFrame = 0;
-            animation.curAnim.stop();
-            nodes = null;
         }
 
     }
